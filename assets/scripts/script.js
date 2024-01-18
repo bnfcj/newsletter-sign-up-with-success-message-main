515dec6ba6aa2fa84942a8f249baa256556f5bb2
bnfcj <67569485+bnfcj@users.noreply.github.com>
2024-01-19 02:01:13 +0800

    fixed bug

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,5 @@
 "use strict";
 
-document.querySelector(".main-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (emailRegex.test(emailElement.value))
-    window.location.href =
-      "https://bnfcj.github.io/newsletter-sign-up-with-success-message-main/success.html?text=" +
-      emailElement.value;
-  else {
-    if (!emailElement.classList.contains("error-input")) {
-      emailElement.classList.toggle("error-input");
-    }
-  }
-});
 const emailElement = document.querySelector(".main-form-input-email");
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const errorElement = document.querySelector(".email-error");
@@ -23,6 +11,18 @@ emailElement.addEventListener("keyup", function () {
     }
   } else {
     errorElement.style.display = "inline";
+    if (!emailElement.classList.contains("error-input")) {
+      emailElement.classList.toggle("error-input");
+    }
+  }
+});
+document.querySelector(".main-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (emailRegex.test(emailElement.value))
+    window.location.href =
+      "https://bnfcj.github.io/newsletter-sign-up-with-success-message-main/success.html?text=" +
+      emailElement.value;
+  else {
     if (!emailElement.classList.contains("error-input")) {
       emailElement.classList.toggle("error-input");
     }
